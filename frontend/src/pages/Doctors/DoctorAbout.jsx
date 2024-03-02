a4c00d1e940e5e01d432142a9f7fc462b6d9e5bf
ahsas.29 <ahsassrivastava29901@gmail.com>
2024-03-02 22:26:27 +0530

    21:51 Sec

--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -1,7 +1,55 @@
 import React from "react";
+import { formateDate } from "../../utils/formateDate";
 
 const DoctorAbout = () => {
-  return <div>DoctorAbout</div>;
+  return (
+    <div>
+        <div>
+            <h3 className="text-[20px] leading-[30px] text-hadingColor font-semibold flex items-center gap-2">
+                About of
+                <span className="text-irisBlueColor font-bold text-[24px] leading-9">
+                    John Smith
+                </span>
+            </h3>
+            <p className="text__para">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet repellendus mollitia tempora placeat eaque eius quisquam consequuntur facilis corporis est?
+            </p>
+        </div>
+        <div className="mt-12">
+            <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
+                Education
+            </h3>
+            <ul className="pt-4 md:p-5">
+                <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
+                    <div>
+                        <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
+                        {formateDate("23 June, 2012")}
+                        </span>
+                        <p className="text-[16px] leading-6 font-medium text-textColor">
+                            PHD in Surgeon
+                        </p>                    
+                    </div>
+                    <p className="text-[14px] leading-5 font-medium text-textColor">
+                        AIIMS, New Delhi.
+                    </p>
+                </li>
+                <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
+                    <div>
+                        <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
+                            {formateDate("07 July, 2008")}
+                        </span>
+                        <p className="text-[16px] leading-6 font-medium text-textColor">
+                            PHD in Surgeon
+                        </p>                    
+                    </div> 
+                    <p className="text-[14px] leading-5 font-medium text-textColor">
+                        New Apollo Hospital, Chennai.
+                    </p>
+                </li>
+            </ul>
+        </div>
+    </div>
+  );
 };
 
 export default DoctorAbout;
