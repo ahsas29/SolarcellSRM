aef3ae71bc696a8eabe4577683add2f1930ec6f3
ahsas.29 <ahsassrivastava29901@gmail.com>
2024-04-09 18:22:37 +0530

    Done Bisi

--- frontend/src/pages/Efficiency.jsx
+++ frontend/src/pages/Efficiency.jsx
@@ -8,7 +8,8 @@ class Efficiency extends React.Component {
         sComposition: '',
         ngd: '',
         wgd: '',
-        result: {} // New state to store the API response
+        result: {}, // New state to store the API response
+        calculationDone: false // State to track if calculation is done
     }
 
     handleChange = (event) => {
@@ -36,7 +37,7 @@ class Efficiency extends React.Component {
             // Parsing response and assigning values
             const [ISC, JSC, VOC, FF, Efficiency] = response.data.efficiency;
             const result = { ISC, JSC, VOC, FF, Efficiency };
-            this.setState({ result });
+            this.setState({ result, calculationDone: true });
         } catch (error) {
             console.error("API Error:", error);
             // You can handle errors here
@@ -44,30 +45,31 @@ class Efficiency extends React.Component {
     }
 
     render() {
-        const { result } = this.state;
+        const { result, calculationDone } = this.state;
         return (
-            <div>
-                <form onSubmit={this.handleSubmit} style={{ display: 'flex', flexDirection: 'column', maxWidth: '300px', margin: '0 auto' }}>
-                    <select name="selectedOption" value={this.state.selectedOption} onChange={this.handleChange} style={{ marginBottom: '10px' }}>
+            <div className="px-5 lg:px-0">
+                <form onSubmit={this.handleSubmit} className="w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10">
+                    <select name="selectedOption" value={this.state.selectedOption} onChange={this.handleChange} className="w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[22px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer" style={{ marginBottom: '10px' }}>
                         <option value="">Select...</option>
                         <option value="G-Dana">G-Dana</option>
                         <option value="T-Dana">T-Dana</option>
                     </select>
-                    <input type="text" name="numberOfQuantumWell" value={this.state.numberOfQuantumWell} placeholder="Number of Quantum Well" onChange={this.handleChange} style={{ marginBottom: '10px' }} />
-                    <input type="text" name="sComposition" value={this.state.sComposition} placeholder="S Composition" onChange={this.handleChange} style={{ marginBottom: '10px' }} />
-                    <input type="text" name="ngd" value={this.state.ngd} placeholder="NGD" onChange={this.handleChange} style={{ marginBottom: '10px' }} />
-                    <input type="text" name="wgd" value={this.state.wgd} placeholder="WGD" onChange={this.handleChange} style={{ marginBottom: '10px' }} />
-                    <button type="submit" style={{ marginBottom: '10px' }}>Submit</button>
+                    <input type="text" name="numberOfQuantumWell" value={this.state.numberOfQuantumWell} placeholder="Number of Quantum Well" onChange={this.handleChange} className="w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[22px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer" style={{ marginBottom: '10px' }} />
+                    <input type="text" name="sComposition" value={this.state.sComposition} placeholder="S Composition" onChange={this.handleChange} className="w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[22px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer" style={{ marginBottom: '10px' }} />
+                    <input type="text" name="ngd" value={this.state.ngd} placeholder="NGD" onChange={this.handleChange} className="w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[22px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer" style={{ marginBottom: '10px' }} />
+                    <input type="text" name="wgd" value={this.state.wgd} placeholder="WGD" onChange={this.handleChange} className="w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[22px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer" style={{ marginBottom: '10px' }} />
+                    <button type="submit" className="w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg px-4 py-3" style={{ marginBottom: '10px' }}>Submit</button>
                 </form>
                 {/* Displaying result */}
-                {result && Object.keys(result).length > 0 &&
-                    <div>
-                        <h2>Result</h2>
+                {calculationDone && Object.keys(result).length > 0 &&
+                    <div className="w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10">
+                        <h2 className="text-headingColor text-[22px] leading-9 font-bold mb-5">Result</h2>
                         <p>ISC: {result.ISC}</p>
                         <p>JSC: {result.JSC}</p>
                         <p>VOC: {result.VOC}</p>
                         <p>FF: {result.FF}</p>
                         <p>Efficiency: {result.Efficiency}</p>
+                        <p className="text-green-500">Results have been calculated perfectly.</p>
                     </div>
                 }
             </div>
